340f37dee4c6e8656fb0006328b88b52110a6312
grossa <grossa144@gmail.com>
2016-10-03 22:09:06 -0400

    Update and rename FadeUIElement.cs to Scripts/FadeUIElement.cs

--- Scripts/FadeUIElement.cs
+++ Scripts/FadeUIElement.cs
@@ -4,15 +4,9 @@ using System.Collections;
 public class FadeUIElement : MonoBehaviour {
 
     public float WaitTime;
-	// Use this for initialization
+
 	void Start ()
     {
             GameObject.Destroy(gameObject, 5.0f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
     }
 }
