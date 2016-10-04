0ac4120352d2497faa78882f6645951a33665fb3
grossa <grossa144@gmail.com>
2016-10-03 22:01:57 -0400

    Update CenterUIElement.cs

--- CenterUIElement.cs
+++ CenterUIElement.cs
@@ -4,13 +4,6 @@ using System.Collections;
 public class CenterUIElement : MonoBehaviour {
 
     public GameObject LeftDome180;
-
-    // Use this for initialization
-    void Start ()
-    {
-        //transform.eulerAngles = new Vector3(LeftDome180.transform.eulerAngles.x, LeftDome180.transform.eulerAngles.y, 0);
-        //transform.position = new Vector3(LeftDome180.transform.position.x, LeftDome180.transform.position.y, LeftDome180.transform.position.z);
-    }
 	
 	// Update is called once per frame
 	void Update ()
